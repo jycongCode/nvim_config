cbd207b78e1203fa94dd078e60f4d69c026ebb49
jycongCode <jicong@ucsd.edu>
2025-05-20 13:00:21 -0700

    INITIAL_COMMIT

---
+++ lua/config/shell.lua
@@ -0,0 +1,11 @@
+-- Example for PowerShell Core
+-- vim.opt.shell = "pwsh"
+-- vim.opt.shellcmdflag = "-NoLogo -NoProfile -Command"
+-- vim.opt.shellquote = ""
+-- vim.opt.shellxquote = ""
+
+-- Or for Git Bash
+vim.opt.shell = [[D:\Git\bin\bash.exe]]
+vim.opt.shellcmdflag = "-c"
+vim.opt.shellquote = '"'
+vim.opt.shellxquote = '"'
---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "tokyonight", -- or your preferred one
+    },
+  },
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      transparent = true,
+    },
+  },
+}
